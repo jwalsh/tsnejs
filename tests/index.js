674f534e0b6d2ce76c729904cb8ae0c492e56364
Jason Walsh <j@wal.sh>
2016-12-01 15:52:18 -0600

    Wrapping ES6 classes

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,12 @@
-import tsnejs from '../src/index';
+import * as tsnejs from '../src/index';
 
-const opt = {};
-opt.epsilon = 10; // epsilon is learning rate (10 = default)
-opt.perplexity = 30; // roughly how many neighbors each point influences (30 = default)
-opt.dim = 2; // dimensionality of the embedding (2 = default)
+console.log(tsnejs);
 
+const opt = {
+  epsilon: 10,    // epsilon is learning rate (10 = default)
+  perplexity: 30, // roughly how many neighbors each point influences (30 = default)
+  dim: 2 // dimensionality of the embedding (2 = default)
+};
 
 const tsne = new tsnejs.tSNE(opt); // create a tSNE instance
 
@@ -12,9 +14,8 @@ const tsne = new tsnejs.tSNE(opt); // create a tSNE instance
 const dists = [[1.0, 0.1, 0.2], [0.1, 1.0, 0.3], [0.2, 0.1, 1.0]];
 tsne.initDataDist(dists);
 
-for(let k = 0; k < 500; k++) {
-  tsne.step(); // every time you call this, solution gets better
-}
+// every time you call this, solution gets better
+// [...Array(500)].forEach((_, i) => tsne.step());
 
 const Y = tsne.getSolution(); // Y is an array of 2-D points that you can plot
 
